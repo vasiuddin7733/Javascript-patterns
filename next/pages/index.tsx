a68ffd9724221e7cfe89d14da945796b4c765f3a
vasiuddin7733 <vasiuddin7733>
2023-11-17 22:27:15 +0100

    add tailwind css file

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import clientPromise from '../lib/mongodb'
 import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
 import "../style/index.module.css"
+import React from 'react'
 
 type ConnectionStatus = {
   isConnected: boolean
