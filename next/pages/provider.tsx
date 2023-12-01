d8e3c00e3a11238e5cfc3510f409833c32f9e201
vasiuddin7733 <vasiuddin7733>
2023-12-01 13:51:27 +0100

    remove all file in javascript patterns

--- next/pages/provider.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const provider = () => {
-  return (
-    <div>provider</div>
-  )
-}
-
-export default provider
--- next/pages/renderingProps.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const renderingProps = () => {
-  return (
-    <div>renderingProps</div>
-  )
-}
-
-export default renderingProps
