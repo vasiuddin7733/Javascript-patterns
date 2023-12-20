ffa1b2f743549125e19423520d70aa67b1be0d1e
vasiuddin7733 <vasiuddin7733>
2023-12-20 11:37:22 +0100

    update flyout component

---
+++ frontend/src/components/compound/flyoutMenu.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { FlyOut } from "./flyout";
+import "./styles.module.css";
+
+export default function FlyoutMenu() {
+  return (
+    <FlyOut>
+      <FlyOut.Toggle />
+      <FlyOut.List>
+        <FlyOut.Item>Edit</FlyOut.Item>
+        <FlyOut.Item>Delete</FlyOut.Item>
+      </FlyOut.List>
+    </FlyOut>
+  );
+}
---
+++ frontend/src/pages/compound.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ImageList from "@/components/compound/images";
+import "../components/compound/styles.module.css";
+
+const compound = () => {
+  return <ImageList />;
+};
+
+export default compound;
